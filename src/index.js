f6c62bd39ace9f32fcbc45135c7aaefccbd0b253
NataliaMelnik <bukato.natallia@gmail.com>
2021-03-14 22:20:22 +0300

    feat: add implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let br = bracketsConfig.flat(Infinity);
+  let st = [];
+  for (var i = 0; i < str.length; ++i) {
+    let ch = str[i];
+    let ind = br.indexOf(ch);
+    let duplicationBrace = br[ind] === br[ind + 1];
+    if (ind >= 0) {
+      if (ind & 1) {
+        if (!st.length) return false;
+        let last_br = st.pop();
+        if (last_br != br[ind - 1]) return false;
+      } else {
+        if (duplicationBrace && st[st.length - 1] === ch) {
+          st.pop();
+        } else {
+          st.push(ch);
+        }
+      }
+    }
+  }
+  return !st.length;
 }
